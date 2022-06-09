c66379893261418b1a9f0f2a06a24753b0ebaa06
Basudev Tyagi <tyagi101basudev@gmail.com>
2022-06-09 10:15:22 +0530

    added code for recognition page

--- web-app/src/main/views.py
+++ web-app/src/main/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/index.html', context)
+    return render(request, 'main/index.html')
+
+
+def recognition(request):
+    return render(request, 'main/recognition.html')
